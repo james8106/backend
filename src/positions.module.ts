9061e8743679ab3767c329d8863491400f274bd0
james8106 <hbulawan@gbox.ncf.edu.ph>
2025-11-23 19:59:01 +0800

    Add Positions module and controller

---
+++ src/positions.module.ts
@@ -0,0 +1,8 @@
+// positions.module.ts
+import { Module } from '@nestjs/common';
+import { PositionsController } from './positions.controller';
+
+@Module({
+  controllers: [PositionsController],
+})
+export class PositionsModule {}
